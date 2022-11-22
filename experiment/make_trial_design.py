cc2b5a58c70e83b08466ea86c3ddac39958e618a
mrenke <maikerenkert95@gmail.com>
2022-11-22 16:23:46 +0100

    put make_trial design (& associated function create_design_magJudge) from utils.py to own file: make_trial_design.py

--- experiment/make_trial_design.py
+++ experiment/make_trial_design.py
@@ -1,3 +1,4 @@
+# runs as; python make_trial_design.py $subject 1 1
 import argparse
 import os.path as op
 import os
@@ -5,55 +6,70 @@ import seaborn as sns
 import pandas as pd
 import numpy as np
 import scipy.stats as ss
-import statsmodels.api as sm
-from utils import create_design, fit_psychometric_curve
 import matplotlib.pyplot as plt
+from itertools import product
+
 
 def main(subject, session=None, run=None):
 
-    log_file = op.abspath(op.join('logs', f'sub-{subject}'))
-
-    if session:
-    	log_file = op.join(log_file, f'ses-{session}')
-
-    log_file = op.join(log_file, f'sub-{subject}')
-
-    if session:
-    	log_file += f'_ses-{session}'
-
-    log_file += '_task-calibration'
-
-    if run:
-    	log_file += f'_run-{run}'
-
-    log_file += '_events.tsv'
-
-    x_lower, x_upper = fit_psychometric_curve(log_file, plot=True)
-
-    print(log_file)
-
- 
-
-    print(x_lower, x_upper)
-    fractions = np.exp(np.linspace(np.log(x_lower), np.log(x_upper), 6, True))
-    base = np.array([7, 10, 14, 20, 28])
-    prob1 = [1., .55]
-    prob2 = [.55, 1.]
-    repetition = (1, 2)
-
+    ## make_trial_design (.tsv fiel already exists for risky-choice task from running calibration, needs to be created newly here)
     task_settings_folder = op.abspath(op.join('settings', 'task'))
     if not op.exists(task_settings_folder):
         os.makedirs(task_settings_folder)
 
     fn = op.abspath(op.join(task_settings_folder,
-                            f'sub-{subject}_ses-task.tsv'))
+                            f'sub-{subject}_ses_task-magJudge.tsv'))
 
-    df = create_design(prob1, prob2, fractions, base=base, repetitions=2, n_runs=6)
+    frac = np.linspace(-6,6, 13)
+    frac = np.delete(frac, 6)                     
+    fractions = np.power(2,(frac/4))
 
+    base = np.array([5, 7, 10, 14, 20])
+
+    df = create_design_magJudge(fractions, base=base, repetitions=3, n_runs=10)
     print(df)
-
     df.to_csv(fn, sep='\t')
-    plt.show()
+
+
+def create_design_magJudge(fractions, base=[5, 7, 10, 14, 20], repetitions=3, n_runs=6):
+
+    base = np.array(base)
+    repetition = range(1, repetitions+1)
+
+    df = []
+    
+    tmp = pd.DataFrame(product(base, fractions, repetition), columns=['base number', 'fraction', 'repetition'])
+
+    df.append(tmp)
+
+    df = pd.concat(df).reset_index(drop=True)
+
+    df['n1'] = df['base number']
+    df['n2'] = df['base number'].astype(int) * df['fraction']
+
+    df['n1'] = df['n1'].astype(int)
+    df['n2'] = df['n2'].astype(int)
+
+    # note df['n1']==df['n2'] for small basenumbers (even though 1 (as frac=0) is removed from fractions)
+            
+    df = df.sample(frac=1)
+
+    # Get run numbers
+    runs = []
+    for x in range(1,n_runs+1):
+        runs.append(np.repeat(x,len(df)/n_runs))
+
+    df['run'] = np.concatenate(runs)
+
+    df = df.set_index(['run'])
+    ixs = np.random.permutation(df.index.unique())
+    df = df.loc[ixs].sort_index(
+        level='run', sort_remaining=False)
+
+    df['trial'] = np.arange(1, len(df)+1)
+
+    return df    
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
