cdea7525c16da0b213d3d8f42df501bc7bfe8c78
mrenke <maikerenkert95@gmail.com>
2022-11-22 16:24:54 +0100

    put make_trial design (& associated function create_design_magJudge) from utils.py to own file: make_trial_design.py

--- experiment/utils.py
+++ experiment/utils.py
@@ -18,24 +18,6 @@ def run_experiment(session_cls, task, use_runs=False, subject=None, session=None
     cmd_args = parser.parse_args()
     subject, session, run, settings = cmd_args.subject, cmd_args.session, cmd_args.run, cmd_args.settings
 
-    ## make_trial_design (.tsv fiel already exists for risky-choice task from running calibration, needs to be created newly here)
-    task_settings_folder = op.abspath(op.join('settings', 'task'))
-    if not op.exists(task_settings_folder):
-        os.makedirs(task_settings_folder)
-
-    fn = op.abspath(op.join(task_settings_folder,
-                            f'sub-{subject}_ses_task-magJudge.tsv'))
-
-    frac = np.linspace(-6,6, 13)
-    frac = np.delete(frac, 6)                     
-    fractions = np.power(2,(frac/4))
-
-    base = np.array([5, 7, 10, 14, 20])
-
-    df = create_design_magJudge(fractions, base=base, repetitions=3, n_runs=10)
-    print(df)
-    df.to_csv(fn, sep='\t')
-    ##
 
     if subject is None:
         subject = input('Subject? (999): ')
@@ -136,43 +118,5 @@ def get_output_dir_str(subject, session, task, run):
 
     return output_dir, output_str
 
-#------------
 
-def create_design_magJudge(fractions, base=[5, 7, 10, 14, 20], repetitions=3, n_runs=6):
 
-    base = np.array(base)
-    repetition = range(1, repetitions+1)
-
-    df = []
-    
-    tmp = pd.DataFrame(product(base, fractions, repetition), columns=['base number', 'fraction', 'repetition'])
-
-    df.append(tmp)
-
-    df = pd.concat(df).reset_index(drop=True)
-
-    df['n1'] = df['base number']
-    df['n2'] = df['base number'].astype(int) * df['fraction']
-
-    df['n1'] = df['n1'].astype(int)
-    df['n2'] = df['n2'].astype(int)
-
-    # note df['n1']==df['n2'] for small basenumbers (even though 1 (as frac=0) is removed from fractions)
-            
-    df = df.sample(frac=1)
-
-    # Get run numbers
-    runs = []
-    for x in range(1,n_runs+1):
-        runs.append(np.repeat(x,len(df)/n_runs))
-
-    df['run'] = np.concatenate(runs)
-
-    df = df.set_index(['run'])
-    ixs = np.random.permutation(df.index.unique())
-    df = df.loc[ixs].sort_index(
-        level='run', sort_remaining=False)
-
-    df['trial'] = np.arange(1, len(df)+1)
-
-    return df    
