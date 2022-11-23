b8aab9dba103ec825634b9e0333c121bd5098145
mrenke <maikerenkert95@gmail.com>
2022-11-23 18:32:50 +0100

    pretty gamble

--- experiment_risk/trial.py
+++ experiment_risk/trial.py
@@ -1,6 +1,6 @@
 from exptools2.core import Trial
 from stimuli import _create_stimulus_array, CertaintyStimulus, ProbabilityPieChart
-from psychopy.visual import TextStim
+from psychopy.visual import TextStim, Circle
 import numpy as np
 
 
@@ -24,17 +24,22 @@ class GambleTrial(Trial):
 
         piechart_width = self.session.settings['various'].get('piechart_width')
 
-        piechart_pos1 = -2, 5
-        piechart_pos2 = 2, 5
+        center_l = -6
+        center_r = 6
+        piechart_pos1 = center_l, 5
+        piechart_pos2 = center_r, 5
 
         self.piechart1 = ProbabilityPieChart(
             self.session.win, prob1, pos=piechart_pos1, size= piechart_width)
         self.piechart2 = ProbabilityPieChart(
             self.session.win, prob2, pos=piechart_pos2, size= piechart_width)
 
+        y_pos = -1
+        pile1_pos = center_l, y_pos
+        pile2_pos = center_r, y_pos
+        #circle_radius =  self.session.settings['pile'].get('aperture_radius')
+        circle_radius = 2.5
 
-        pile1_pos = -2, 0
-        pile2_pos = 2, 0
         self.pile1 = _create_stimulus_array(self.session.win,
                                             num1,
                                             self.session.settings['pile'].get(
@@ -46,22 +51,24 @@ class GambleTrial(Trial):
 
         self.pile2 = _create_stimulus_array(self.session.win,
                                             num2,
-                                            self.session.settings['pile'].get(
-                                                'aperture_radius'),
+                                            circle_radius,
                                             self.session.settings['pile'].get(
                                                 'dot_radius'),
                                                 pos=pile2_pos,
                                             image=self.session.image1)
+        self.circle1 = Circle(self.session.win, radius = circle_radius + 2, pos = pile1_pos, lineColor= 'white' , fillColor = None, lineWidth = 4 )
+        self.circle2 = Circle(self.session.win, radius = circle_radius + 2, pos = pile2_pos, lineColor= 'white' , fillColor = None, lineWidth = 4 )
 
-
+        self.fixation_cross = TextStim(self.session.win, '+', color=(1., 1., 1.), pos = (0,y_pos))
 
     def draw(self):
-        self.session.fixation_cross.draw()
+        self.fixation_cross.draw()
         self.piechart1.draw()
         self.piechart2.draw()
         self.pile1.draw()
         self.pile2.draw()
-
+        self.circle1.draw()
+        self.circle2.draw()
 
     def get_events(self):
 
