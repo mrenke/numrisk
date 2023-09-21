7567f9732da73a377c49a230ac22a1965a74b44e
mrenke <maikerenkert95@gmail.com>
2023-09-21 16:03:54 +0200

    glm_denoise folder structure and submit script

---
+++ numrisk/fmri_analysis/glm_denoise/submit_glm_mr.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --job-name=fit_st_denoise
+#SBATCH --output=/home/mrenke/logs/fit_st_denoise_%A-%a.txt
+#SBATCH --ntasks=1
+#SBATCH -c 16
+#SBATCH --time=2:00:00
+
+. $HOME/.bashrc
+
+source activate numrefields
+
+export PARTICIPANT_LABEL=$(printf "%02d" $SLURM_ARRAY_TASK_ID)
+
+python $HOME/git/numrisk/numrisk/fmri_analysis/glm/fit_glm_denoise.py $PARTICIPANT_LABEL --bids_folder /shares/zne.uzh/mrenke/ds-dnumrisk
