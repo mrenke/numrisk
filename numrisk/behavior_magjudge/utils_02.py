d55a8ef464ae2a4db09079f2977968626bc69b80
mrenke <maikerenkert95@gmail.com>
2024-06-05 16:17:28 +0200

    behavior more and more (simple?:P)

--- numrisk/behavior_magjudge/utils_02.py
+++ numrisk/behavior_magjudge/utils_02.py
@@ -9,17 +9,28 @@ import arviz as az
 import matplotlib.pyplot as plt
 import seaborn as sns
 import scipy.stats as ss
+from bauer.utils.bayes import softplus
 
-def get_subwise_params(idata, param_name):
+def get_subwise_params(idata, param_name, group_list=None):
     df_param= idata.posterior[param_name].to_dataframe()    
     df_param.columns.name = 'parameter'
     df_param.index = df_param.index.set_names(['chain','draw','subject','regressor']) 
     df_param = df_param.stack().to_frame('value')
 
-    df_param = df_param.xs('Intercept', 0,'regressor')
+    if group_list is not None:
+        df_param = df_param.join(group_list, on='subject')
+        df_param_int = df_param.xs('Intercept', 0,'regressor')
+        df_param_group = df_param.xs('group', 0,'regressor')
+        df_result = df_param_int.copy()
+        df_result.loc[df_result['group'] == 1] += df_param_group.loc[df_result['group'] == 1]
+        df_param = df_result
+    else:
+        df_param = df_param.xs('Intercept', 0,'regressor')
     df_param = df_param.groupby(['subject'])[['value']].mean()
     df_param = df_param.rename(mapper={'value':param_name},axis=1)
-
+    
+    if 'sd' in param_name:
+        df_param[param_name]= softplus(df_param[param_name]) 
     return df_param
 
 def build_model(model_label, df):
